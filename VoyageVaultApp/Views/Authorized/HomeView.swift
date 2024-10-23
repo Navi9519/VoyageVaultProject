85b6900d91f973041051298184f0876aa7c979c3
Nicholas <144162774+Clemond@users.noreply.github.com>
2024-10-23 13:48:16 +0200

    Feature/home view (#11)
    * Changed the name of 'secureTxtFieldComponent' to 'SecureTxtFieldComponent'
    
    * Added assets to img folder in assets, Added background picture to homeView
    
    * Added new colorSets to assets, added name and profile image design to homeView
    
    * Changed design in CardComponent
    
    * Added CardComponents and next trip timer on homeView
    
    * Made the CardComponent into a NavigationLink, it is now clickable

--- VoyageVaultApp/Views/Authorized/HomeView.swift
+++ VoyageVaultApp/Views/Authorized/HomeView.swift
@@ -9,7 +9,67 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("This is HomeView")
+        ZStack {
+            Image("background_pic")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                HStack {
+                    
+                    VStack (alignment: .leading){
+                        Text("First name")
+                            .font(.title2)
+                        Text("Surname")
+                            .bold()
+                            .font(.title)
+                            .padding(.top,0.5)
+                    }
+                    
+                    Spacer()
+                    
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .frame(width: 75,height: 75)
+                    
+                }
+                .frame(maxWidth: .infinity)
+                .padding(30)
+                .padding(.top,30)
+                
+                VStack {
+                    Text("Time until your next trip: ")
+                        .foregroundStyle(.white)
+                        .font(.title3)
+                    Text("24 days, 16 hours and 5 min")
+                        .foregroundStyle(.white)
+                }
+                .shadow(radius: 10)
+                
+                
+
+                Spacer()
+                
+                VStack (spacing: 30){
+                    CardComponent(title: "Upcoming Trips", description: "Join the Forum to connect with fellow travelers, share tips, and discover new adventures!", image: "imgCalender", color1: Color("beigeColorOne"), color2: Color("beigeColorTwo"),destination: {LandingView()})
+                    
+                    CardComponent(title: "Friend list", description: "Manage your Friend List to stay connected and explore the world together with your travel buddies!", image: "imgFriendList", color1: Color("orangeColorOne"), color2: Color("orangeColorTwo"),destination: {LandingView()})
+                    
+                    CardComponent(title: "Explore", description: "Tap Explore to discover exciting destinations, activities, and hidden gems around the world!", image: "imgExplore", color1: Color("blueColorOne"), color2: Color("blueColorTwo"),destination: {LandingView()})
+                }.shadow(radius: 10)
+                
+                
+                Spacer()
+                
+                
+            }
+                
+          
+                
+            }
+        
     }
 }
 
