db36b43189b42b3cfb3f39de3ad3176ecf640bea
Nicholas <quackshows11@gmail.com>
2024-10-23 11:55:40 +0200

    Added new colorSets to assets, added name and profile image design to homeView

--- VoyageVaultApp/Views/Authorized/HomeView.swift
+++ VoyageVaultApp/Views/Authorized/HomeView.swift
@@ -15,7 +15,35 @@ struct HomeView: View {
                 .scaledToFill()
                 .ignoresSafeArea()
             
-            VStack() {
+            VStack {
+                
+                HStack {
+                    
+                    VStack (alignment: .leading){
+                        Text("First name")
+                            .font(.title2)
+                        Text("Surname")
+                            .bold()
+                            .font(.title)
+                            .padding(.top,0.5)
+                    }
+                    
+                    Spacer()
+                    
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .frame(width: 75,height: 75)
+                    
+                }
+                .frame(maxWidth: .infinity)
+                .padding(30)
+                .background(.gray)
+                .padding(.vertical,30)
+                
+                Spacer()
+                
+                CardComponent(title: "Upcoming Trips", description: "Join the Forum to connect with fellow travelers, share tips, and discover new adventures!", image: "imgCalender", color1: Color("beigeColorOne"), color2: Color("beigeColorTwo"))
+                
                 
             }
                 
