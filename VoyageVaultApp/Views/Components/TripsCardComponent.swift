961f86186305813c3be6eacfc4a45a8dd441a888
Nicholas <quackshows11@gmail.com>
2024-10-24 10:18:55 +0200

    Added design to TripsCardComponent

--- VoyageVaultApp/Views/Components/TripsCardComponent.swift
+++ VoyageVaultApp/Views/Components/TripsCardComponent.swift
@@ -8,9 +8,12 @@
 import SwiftUI
 
 struct TripsCardComponent: View {
-    var title: String
-    var description: String
-    var image: String
+    var country: String
+    var city: String
+    var population: Int
+    var currency: String
+    var flag: String
+    var daysUntilTrip: Int
     
     var color1: Color
     var color2: Color
@@ -27,42 +30,83 @@ struct TripsCardComponent: View {
                 LinearGradient(colors: [color1, color2], startPoint: .leading, endPoint: .trailing)
                 
                 
-                HStack (spacing: 10) {
-                    Image(image)
-                        .resizable()
-                        .frame(width: 59, height: 59)
-                        .shadow(radius: 5)
+                VStack (spacing: 30) {
                     
-                    VStack (spacing: 10) {
-                        Text(title)
-                            .foregroundStyle(.black)
-                            .font(.system(size: 25))
-                            .fontWeight(.bold)
-                            .multilineTextAlignment(.center)
+                    Spacer()
+
+                    HStack {
+            
+                        Image(flag)
+                            .resizable()
+                            .frame(width: 80, height: 80)
+                            .shadow(radius: 5)
+                            .padding(.top)
+                            .padding(.horizontal,30)
+
                         
-                        Text(description)
-                            .foregroundStyle(.black)
-                            .italic()
-                            .font(.system(size: 15))
-                            .fontWeight(.semibold)
-                            .frame(alignment: .center)
-                            .multilineTextAlignment(.center)
+                        VStack (spacing: 10) {
+                            Text(country)
+                                .foregroundStyle(.black)
+                                .font(.system(size: 40))
+                                .fontWeight(.bold)
+                                .multilineTextAlignment(.center)
+                            
+                            Text("in \(daysUntilTrip) days")
+                                .foregroundStyle(.black)
+                                .italic()
+                                .font(.system(size: 18))
+                                .fontWeight(.semibold)
+                                .frame(alignment: .center)
+                                .multilineTextAlignment(.center)
+                        }
+                        .padding(.horizontal,20)
                         
+                        Spacer()
                     }
-                    .padding(.horizontal,20)
-                    //.padding(.vertical,10) // Optional
+                    
+                    HStack(alignment: .bottom){
+                        VStack (alignment: .leading){
+                            Text("Country: \(country)").font(.title3).bold()
+                            Text("Population: \(population)").font(.title3).bold()
+                            Text("Currency: \(currency)").font(.title3).bold()
+                            
+                            
+                        }
+                        
+                        Spacer()
+                        
+                        HStack (spacing: 15){
+                            Image(systemName: "pencil")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                            Image(systemName: "trash")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                        }
+                       
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal,30)
+                    
+                    
+                    
+                    Spacer()
+                    
                 }
-                .padding()
             }
-            .frame(maxWidth: dynamicScreenWidth * 0.90, maxHeight: dynamicScreenHeight * 0.18) // Frame for the entire card
+            .frame(maxWidth: dynamicScreenWidth * 0.90, maxHeight: dynamicScreenHeight * 0.25) // Frame for the entire card
+            .clipShape(.rect(cornerRadius: 10))
             .overlay(
-                    RoundedRectangle(cornerRadius: 10)  // Shape of the border
-                        .stroke(Color.black, lineWidth: 1)  // Blue border with width 2
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.black, lineWidth: 1)
                 )
+        
         
     }
 }
 
 #Preview {
-    TripsCardComponent(title: "title", description: "Description", image: "imgBackHome", color1: Color("beigeColorOne"), color2: Color("beigeColorTwo"))
+    TripsCardComponent(country: "Spain", city: "Malaga", population: 571_026, currency: "Euro", flag: "spain", daysUntilTrip: 24, color1: Color("beigeColorOne"), color2: Color("beigeColorTwo"))
 }
+
+
