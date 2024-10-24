9938d5bb9c67a9477ead348c83f1f3bc7d877bb2
Nicholas <144162774+Clemond@users.noreply.github.com>
2024-10-24 15:11:01 +0200

    Started implementing country api (#18)

--- VoyageVaultApp/Models/Api/Api.swift
+++ VoyageVaultApp/Models/Api/Api.swift
@@ -9,6 +9,41 @@ import Foundation
 
 class Api {
     
+    func post<T:Encodable, R:Decodable>(url: String, body: T?) async throws -> R {
+        
+        guard let url = URL(string: url) else { throw APIError.invalidURL}
+        
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "POST"
+        
+        if let body = body {
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        }
+        
+        let encoder = JSONEncoder()
+        
+        do {
+            request.httpBody = try encoder.encode(body)
+        }  catch {
+            throw APIError.invalidRequest
+        }
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode >= 200 && httpResponse.statusCode < 205 else { throw APIError.invalidResponse }
+        
+        do {
+            let decoder = JSONDecoder()
+            
+            let decodedresponse = try decoder.decode(R.self, from: data)
+            return decodedresponse
+        } catch {
+            throw APIError.invalidData
+        }
+    }
+    
+    
     func get<R: Decodable>(url: String) async throws -> R {
         
         //Validera URL
@@ -40,4 +75,5 @@ enum APIError: Error {
     case invalidURL
     case invalidResponse
     case invalidData
+    case invalidRequest
 }
--- VoyageVaultApp/Views/Authorized/UpcomingTripsView.swift
+++ VoyageVaultApp/Views/Authorized/UpcomingTripsView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct UpcomingTripsView: View {
+    
+    @StateObject var countryManager = CountryManager()
+    
     var body: some View {
         ZStack {
             Image("background_pic")
@@ -45,7 +48,7 @@ struct UpcomingTripsView: View {
                 
                 VStack (spacing: 30){
                     
-                    TripsCardComponent(country: "Spain", city: "Malaga", population: 245_345, currency: "Euro", flag: "spain", daysUntilTrip: 24, color1: Color("beigeColorOne"), color2: Color("beigeColorTwo"))
+                    TripsCardComponent(country: "Spain", city: "Malaga", population: 245_345, currency: "Euro", flag: "\(countryManager.country?.flag ?? "spain")", daysUntilTrip: 24, color1: Color("beigeColorOne"), color2: Color("beigeColorTwo"))
                     
                     TripsCardComponent(country: "Ukraine", city: "Kiev", population: 2_900_000, currency: "ryvnia", flag: "ukraine", daysUntilTrip: 142, color1: Color("orangeColorOne"), color2: Color("orangeColorTwo"))
                 }
@@ -54,6 +57,19 @@ struct UpcomingTripsView: View {
                 Spacer()
                 
                 NavLinkComponent(text: "Add new trip", width: 150, height: 50, destination: {AddNewTripView()}).shadow(radius: 10)
+                
+                //Test button
+                Button("test", action: {
+                    Task {
+                        do {
+                            
+                            try await countryManager.getCountryByISO(iso: "NG")
+                            
+                        } catch let error{
+                            print(error.localizedDescription)
+                        }
+                    }
+                })
                 
                 Spacer()
                 
