b268c2e635cc05d401e46377d15276c970b2dc3d
Navi9519 <105633246+Navi9519@users.noreply.github.com>
2024-10-25 11:23:00 +0200

    created folder for card components, created initial profileView with corresponding values from loged in user

---
+++ VoyageVaultApp/Views/Authorized/ProfileView.swift
@@ -0,0 +1,112 @@
+//
+//  ProfileView.swift
+//  VoyageVaultApp
+//
+//  Created by Ivan Dedic on 2024-10-25.
+//
+
+import Foundation
+
+
+import SwiftUI
+
+struct ProfileView: View {
+    
+    
+    @EnvironmentObject var firebaseAuth: FirebaseAuth
+    
+    
+    var body: some View {
+        ZStack {
+            Image("background_pic")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                if let currentUserData = firebaseAuth.currentUserData  {
+                    
+                    HStack {
+                        
+                       
+                        
+                        VStack (alignment: .leading){
+                           
+                            Text(currentUserData.firstName)
+                                .font(.title2)
+                           
+                            Text(currentUserData.surName)
+                                .bold()
+                                .font(.title)
+                            
+                            Text("Age: \(currentUserData.age)")
+                                .bold()
+                                .font(.title2)
+                            
+                            Text("Nationality: \(currentUserData.nationality)")
+                                .bold()
+                                .font(.title2)
+                            
+                                
+                        }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            firebaseAuth.signOutUser()
+                        }, label: {
+                            Image(systemName: "person.circle")
+                                .resizable()
+                                .frame(width: 70,height: 70)
+                        }).foregroundStyle(.black)
+
+                        
+                       
+                        
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(30)
+                    .padding(.top,30)
+                    
+                    VStack {
+                        Text("Time until your next trip: ")
+                            .foregroundStyle(.white)
+                            .font(.title3)
+                        Text("24 days, 16 hours and 5 min")
+                            .foregroundStyle(.white)
+                    }
+                    .shadow(radius: 10)
+                    
+                    
+
+                    Spacer()
+                    
+                    VStack (spacing: 30){
+                        HomeCardComponent(title: "Upcoming Trips", description: "Join the Forum to connect with fellow travelers, share tips, and discover new adventures!", image: "imgCalender", color1: Color("beigeColorOne"), color2: Color("beigeColorTwo"),destination: {UpcomingTripsView()})
+                        
+                        HomeCardComponent(title: "Friend list", description: "Manage your Friend List to stay connected and explore the world together with your travel buddies!", image: "imgFriendList", color1: Color("orangeColorOne"), color2: Color("orangeColorTwo"),destination: {FriendListView()})
+                        
+                        HomeCardComponent(title: "Explore", description: "Tap Explore to discover exciting destinations, activities, and hidden gems around the world!", image: "imgExplore", color1: Color("blueColorOne"), color2: Color("blueColorTwo"),destination: {ExploreView()})
+                    }.shadow(radius: 10)
+                    
+                    
+                    Spacer()
+                    
+                    
+                }
+                    
+                }
+                
+               
+                
+          
+                
+            }
+        
+    }
+}
+
+#Preview {
+    ProfileView().environmentObject(FirebaseAuth())
+}
