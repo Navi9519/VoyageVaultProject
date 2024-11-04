267c9c13a1e368f97b73bb3b4ba189418454a633
Nicholas <quackshows11@gmail.com>
2024-11-04 10:22:45 +0100

    Created a copy of ProfileView which will act as a FriendProfileView

---
+++ VoyageVaultApp/Views/Authorized/FriendProfileView.swift
@@ -0,0 +1,110 @@
+//
+//  FriendProfileView.swift
+//  VoyageVaultApp
+//
+//  Created by Nicholas Nieminen on 2024-11-04.
+//
+
+import SwiftUI
+
+struct FriendProfileView: View {
+    
+    @EnvironmentObject var firebaseAuth: FirebaseAuth
+    @ObservedObject var storage: Firestorage
+    
+    @State var isPickerShowing = false
+    
+    var body: some View {
+        ZStack {
+            Image("background_pic")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                if let currentUserData = firebaseAuth.currentUserData  {
+                    
+                    HStack {
+                        VStack (alignment: .leading){
+                           
+                            Text(currentUserData.firstName)
+                                .font(.title2)
+
+                            Text(currentUserData.surName)
+                                .bold()
+                                .font(.title)
+                            
+                            Text("Age: \(currentUserData.age)")
+                                .bold()
+                                .font(.title2)
+                            
+                            Text("Nationality: \(currentUserData.nationality)")
+                                .bold()
+                                .font(.title2)
+                        }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            firebaseAuth.signOutUser()
+                        }, label: {
+                            Image(systemName: "person.circle")
+                                .resizable()
+                                .frame(width: 70,height: 70)
+                        })
+                        .foregroundStyle(.black)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(30)
+                    .padding(.top,30)
+                    
+                    VStack (spacing: 30){
+                        
+                        FavoriteDestinationsCardComponent(
+                            title: "\(currentUserData.firstName)'s favorite cities:",
+                            cities: [
+                                CityTest(id: 1, name: "Prague", flag: "🇨🇿"),
+                                CityTest(id: 2, name: "Berlin", flag: "🇩🇪"),
+                                CityTest(id: 3, name: "Tokyo", flag: "🇯🇵")
+                            ],
+                            color1: Color("beigeColorOne"),
+                            color2: Color("backgroundTwo"))
+                        
+                        ImageVaultCardComponent(
+                            title: "\(currentUserData.firstName)'s vault",
+                            color1: Color("orangeColorOne"),
+                            color2: Color("orangeColorTwo"),
+                            addNewPic: {
+                                isPickerShowing = true
+                            },
+                            retrievedImages: $storage.retrievedImages)
+                        
+                        if storage.selectedImage != nil {
+                            
+                            Button(action: {
+                                // Upload the image
+                                storage.uploadFoto()
+                                
+                            }, label: {
+                                Text("Upload photo")
+                            })
+                            
+                        }
+                        Spacer()
+                    }
+                    .shadow(radius: 10)
+                }
+            }
+        }.sheet(isPresented: $isPickerShowing, onDismiss: nil) {
+                ImagePicker(selectedImage: $storage.selectedImage, isPickerShowing: $isPickerShowing)
+        }
+        .onAppear {
+            storage.retrievePhotos()
+        }
+    }
+}
+
+#Preview {
+    FriendProfileView(storage: Firestorage(firebase: FirebaseAuth())).environmentObject(FirebaseAuth())
+}
